fc5628d596733501a1434d104d972bf2375087e3
Oliver 2wist <joliver017@gmail.com>
2019-04-27 16:00:37 -0500

    successfully made title a clickable link instead of separate http link

--- public/app.js
+++ public/app.js
@@ -3,7 +3,8 @@ $.getJSON("/articles", function(data) {
   // For each one
   for (var i = 0; i < data.length; i++) {
     // Display the apropos information on the page
-    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br /> <br />" + data[i].teaser + "<br />" + data[i].link + "</p>");
+    $("#articles").append("<p data-id='" + data[i]._id + "'>" + "<a href=" + data[i].link + ">" + data[i].title + "</a>" + "<br /> <br />" + data[i].teaser + "<br />" + 
+     "</p>");
   }
 });
 
